9aeea5a99a2c6390f9a87d20ca2c8819017f8c0b
Jhabriel Varela <jhabriel@gmail.com>
2020-04-12 19:05:43 +0200

    Update documentation + black

--- a_posteriori_error.py
+++ a_posteriori_error.py
@@ -4,13 +4,22 @@ import pressure_reconstruction
 import flux_reconstruction
 import error_evaluation
 
-class PosterioriError():
-    "Parent class for computing A posteriori errors for mixed-dimensional equations"
-    
-    def __init__(self, grid_bucket, parameter_keyword, subdomain_variable, nodal_method="k-averaging", p_order="1"):
+
+class PosterioriError:
+    "Parent class for computing a posteriori errors for mixed-dimensional equations"
+
+    def __init__(
+        self,
+        grid_bucket,
+        parameter_keyword,
+        subdomain_variable,
+        nodal_method="k-averaging",
+        p_order="1",
+    ):
         """
-        Class for computing a posteriori error estimates of mixed-dimensional
-        flow equations
+        Class for computing a posteriori error estimates for mixed-dimensional
+        flow equations. The errors are included under pp.STATE on the dictionaries
+        corresponding to each grid.
 
         Parameters
         ----------
@@ -20,39 +29,42 @@ class PosterioriError():
             Keyword referring to the problem.
         subdomain_variable : string
             Keyword referring to the subdomain variable.
-        nodal_method : TYPE, optional
-            DESCRIPTION. The default is "k-averaging".
-        p_order : TYPE, optional
-            DESCRIPTION. The default is "1".
+        nodal_method : string, optional
+            Method for obtaining nodal pressures. The default is "k-averaging".
+            The other implemented option is "mpfa-inverse".
+        p_order : string, optional
+            Order of polynomial reconstruction. The default is "1". The other
+            implemented option is "1.5", which refers to P1 elements enriched
+            with purely parabolic terms.
 
         Returns
         -------
-        None.
+        None. 
 
         """
         self.gb = grid_bucket
         self.kw = parameter_keyword
         self.sdv = subdomain_variable
-        
-        # Loop through the nodes
-        for g, d in self.gb:          
-            
+
+        # Compute the errors on the subdomains by looping through the nodes
+        for g, d in self.gb:
+
             # Rotate grid to reference coordinate system
             g_rot = self._rotate_grid(g)
             # Get reconstructed pressure coefficients
-            p_coeffs = pressure_reconstruction.subdomain_pressure(g_rot, d, self.kw, self.sdv, nodal_method, p_order)
+            p_coeffs = pressure_reconstruction.subdomain_pressure(
+                g_rot, d, self.kw, self.sdv, nodal_method, p_order
+            )
             # Get reconstructed velocity coefficients
-            v_coeffs, _ = flux_reconstruction.subdomain_velocity(g_rot, d, self.kw)               
-            
-            # Compute different errors
-            error_evaluation.diffusive_flux_sd(g_rot, d, p_coeffs, v_coeffs);
-    
+            v_coeffs, _ = flux_reconstruction.subdomain_velocity(g_rot, d, self.kw)
 
-        # Loop through the edges
+            # Compute different errors
+            error_evaluation.diffusive_flux_sd(g_rot, d, p_coeffs, v_coeffs)
+
+        # Compute the errors on the interfaces by looping through the edges
         for e, d in self.gb.edges():
             # TODO: Calculate errors in the interfaces
             pass
-       
 
     def _rotate_grid(self, g):
         """
@@ -66,22 +78,25 @@ class PosterioriError():
         -------
         g_rot (Porepy object): Rotated grid
         """
-        
+
         # Copy grid to keep original one untouched
-        g_rot = g.copy()  
-        
+        g_rot = g.copy()
+
         # Rotate grid
-        cell_centers, face_normals, face_centers, R, dim, nodes = pp.map_geometry.map_grid(g_rot)
-        
+        (
+            cell_centers,
+            face_normals,
+            face_centers,
+            R,
+            dim,
+            nodes,
+        ) = pp.map_geometry.map_grid(g_rot)
+
         # Update rotated fields in the relevant dimension
         for dim in range(g.dim):
             g_rot.cell_centers[dim] = cell_centers[dim]
             g_rot.face_normals[dim] = face_normals[dim]
             g_rot.face_centers[dim] = face_centers[dim]
             g_rot.nodes[dim] = nodes[dim]
-        
+
         return g_rot
-    
-    
-        
-        
