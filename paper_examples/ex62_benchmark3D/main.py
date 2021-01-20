9a8f0610a7e37737a9c26f196d306337582ea2d6
Jhabriel Varela <jhabriel@gmail.com>
2021-01-20 18:38:21 +0100

    Overal code clean up

--- paper_examples/ex62_benchmark3D/main.py
+++ paper_examples/ex62_benchmark3D/main.py
@@ -7,6 +7,7 @@ import scipy.sparse.linalg as spla
 import itertools
 import mdestimates as mde
 
+
 def make_grid(num):
     """
     Creates mixed-dimensional grid
@@ -32,6 +33,7 @@ def make_grid(num):
 
     return pp.fracture_importer.dfm_from_gmsh(fn, 3)
 
+
 def low_zones(g):
     """
     Returns indices corresponding to the lower zones of the domain
@@ -47,7 +49,7 @@ def low_zones(g):
         Containting the indices.
 
     """
-    
+
     if g.dim < 3:
         return np.zeros(g.num_cells, dtype=np.bool)
 
@@ -171,6 +173,7 @@ def set_parameters_conductive(gb):
 
     return None
 
+
 #%% Input data
 numerical_methods = ["TPFA", "MPFA", "RT0", "MVEM"]
 mesh_resolutions = ["coarse", "intermediate", "fine"]
@@ -178,25 +181,25 @@ subdomain_variable = "pressure"
 subdomain_operator_keyword = "diffusive"
 flux_variable = "flux"
 edge_variable = "mortar_flux"
-    
+
 #%% Initialize dictionary
 out = {k: {} for k in numerical_methods}
 for i in itertools.product(numerical_methods, mesh_resolutions):
     out[i[0]][i[1]] = {
-        "error_node_3d": [ ],
-        "error_node_2d": [ ],
-        "error_node_1d": [ ],
-        "error_edge_2d": [ ],
-        "error_edge_1d": [ ],
-        "error_edge_0d": [ ],
-        "error_global": [ ]
-        }
+        "error_node_3d": [],
+        "error_node_2d": [],
+        "error_node_1d": [],
+        "error_edge_2d": [],
+        "error_edge_1d": [],
+        "error_edge_0d": [],
+        "error_global": [],
+    }
 
 #%% Generate grid bucket
 folder = "geiger_3d"
-conductive = True # Benchmark for the case of conductive domains
+conductive = True  # Benchmark for the case of conductive domains
 for i in itertools.product(numerical_methods, mesh_resolutions):
-    
+
     # Print simulation info in the console
     print("Solving with", i[0], "for", i[1], "mesh size.")
 
@@ -210,26 +213,28 @@ for i in itertools.product(numerical_methods, mesh_resolutions):
     elif i[1] == "intermediate":
         gb = make_grid(1)
     elif i[1] == "fine":
-        gb = make_grid(2)    
+        gb = make_grid(2)
     else:
         raise ValueError(msg)
-    
-    print(f"Grid construction done. Time {time() - tic}")  
-    
+
+    print(f"Grid construction done. Time {time() - tic}")
+
     #%% Solve the problem
     set_parameters_conductive(gb)
-        
+
     if i[0] == "TPFA":
         assembler, block_info = helpers.setup_flow_assembler(gb, pp.Tpfa("flow"))
     elif i[0] == "MPFA":
         assembler, block_info = helpers.setup_flow_assembler(gb, pp.Mpfa("flow"))
     elif i[0] == "RT0":
-         assembler, block_info = helpers.setup_flow_assembler(gb, pp.RT0("flow"))
+        assembler, block_info = helpers.setup_flow_assembler(gb, pp.RT0("flow"))
     elif i[0] == "MVEM":
-         assembler, block_info = helpers.setup_flow_assembler(gb, pp.MVEM("flow"))
+        assembler, block_info = helpers.setup_flow_assembler(gb, pp.MVEM("flow"))
     else:
-         raise ValueError('Numerical method should be either "TPFA", "MPFA", "RT0", or "MVEM".')
-   
+        raise ValueError(
+            'Numerical method should be either "TPFA", "MPFA", "RT0", or "MVEM".'
+        )
+
     tic = time()
     assembler.discretize()
     print(f"Discretization finished. Time {time() - tic}")
@@ -237,7 +242,7 @@ for i in itertools.product(numerical_methods, mesh_resolutions):
     A, b = assembler.assemble_matrix_rhs()
     x = spla.spsolve(A, b)
     assembler.distribute_variable(x)
-    
+
     #%% Overwrite d[pp.STATE][subdomain_variable] to be consistent with FEM methods
     for g, d in gb:
         discr = d[pp.DISCRETIZATION][subdomain_variable][subdomain_operator_keyword]
@@ -245,22 +250,21 @@ for i in itertools.product(numerical_methods, mesh_resolutions):
         flux = discr.extract_flux(g, d[pp.STATE][subdomain_variable], d).copy()
         d[pp.STATE][subdomain_variable] = pressure
         d[pp.STATE][flux_variable] = flux
-    
+
     print(f"Problem succesfully solved. Time {time() - tic}")
-    
+
     #%% Estimate errors
     tic = time()
-    estimates = mde.ErrorEstimate(gb, 
-                                 lam_name=edge_variable, 
-                                 sd_operator_name=subdomain_operator_keyword,
-                                 )
+    estimates = mde.ErrorEstimate(
+        gb, lam_name=edge_variable, sd_operator_name=subdomain_operator_keyword
+    )
     estimates.estimate_error()
     estimates.transfer_error_to_state()
     scaled_majorant = estimates.get_scaled_majorant()
     print(f"Errors succesfully estimated. Time {time() - tic}")
     estimates.print_summary(scaled=True)
     print("\n")
-    
+
     #%% Compute errors
     error_node_3d = 0
     error_node_2d = 0
@@ -272,98 +276,47 @@ for i in itertools.product(numerical_methods, mesh_resolutions):
     # Get subdomain errors
     for g, d in gb:
         if g.dim == 3:
-            #perm = d[pp.PARAMETERS]["flow"]["second_order_tensor"].values
-            #perm = perm[0][0]
-            #error_node_3d += ( (1/perm) * d[pp.STATE]["diffusive_error"]).sum() ** 0.5
             error_node_3d += estimates.get_scaled_local_errors(g, d)
-            #d[pp.STATE]['scaled_error'] = (1/perm) * d[pp.STATE]["diffusive_error"]
         elif g.dim == 2:
-            #perm = d[pp.PARAMETERS]["flow"]["second_order_tensor"].values
-            #perm = perm[0][0]
-            #error_node_2d += ( (1/perm) * d[pp.STATE]["diffusive_error"]).sum() ** 0.5
             error_node_2d += estimates.get_scaled_local_errors(g, d)
-            #d[pp.STATE]['scaled_error'] = (1/perm) * d[pp.STATE]["diffusive_error"]
         elif g.dim == 1:
-            #perm = d[pp.PARAMETERS]["flow"]["second_order_tensor"].values
-            #perm = perm[0][0]
-            #error_node_1d += ( (1/perm) * d[pp.STATE]["diffusive_error"]).sum() ** 0.5
             error_node_1d += estimates.get_scaled_local_errors(g, d)
-            #d[pp.STATE]['scaled_error'] = (1/perm) * d[pp.STATE]["diffusive_error"]
         else:
             continue
-    
+
     # Get interface errors
     for e, d_e in gb.edges():
-        mg = d_e['mortar_grid']
+        mg = d_e["mortar_grid"]
         if mg.dim == 2:
-            #knormal = d[pp.PARAMETERS]["flow"]["normal_diffusivity"]
-            #error_edge_2d += ( (1/knormal) * d[pp.STATE]["diffusive_error"]).sum() ** 0.5
             error_edge_2d += estimates.get_scaled_local_errors(mg, d_e)
-            #d[pp.STATE]['scaled_error'] = (1/knormal) * d[pp.STATE]["diffusive_error"]
         elif mg.dim == 1:
-            #knormal = d[pp.PARAMETERS]["flow"]["normal_diffusivity"]
-            #error_edge_1d += ( (1/knormal) * d[pp.STATE]["diffusive_error"]).sum() ** 0.5
             error_edge_1d += estimates.get_scaled_local_errors(mg, d_e)
-            #d[pp.STATE]['scaled_error'] = (1/knormal) * d[pp.STATE]["diffusive_error"]
         elif mg.dim == 0:
-            #knormal = d[pp.PARAMETERS]["flow"]["normal_diffusivity"]
-            #error_edge_0d += ( (1/knormal) * d[pp.STATE]["diffusive_error"]).sum() ** 0.5
             error_edge_0d += estimates.get_scaled_local_errors(mg, d_e)
-            #d[pp.STATE]['scaled_error'] = (1/knormal) * d[pp.STATE]["diffusive_error"]
-    
-    # # Get global errors
-    # error_global = estimates.compute_global_error() ** 0.5
-    # scaling_factors = [ ]
-    # for g, d in gb:
-    #     V = g.cell_volumes
-    #     if g.dim != 0:
-    #         perm = d[pp.PARAMETERS]["flow"]["second_order_tensor"].values
-    #         perm = perm[0][0]
-    #         scaling_factors.append(np.max(perm))
-    # for e, d in gb.edges():
-    #     mg = d['mortar_grid']
-    #     V = mg.cell_volumes
-    #     k_norm = d[pp.PARAMETERS]["flow"]['normal_diffusivity']
-    #     scaling_factors.append(np.max(k_norm))
-    
-    # scale_factor = np.max(scaling_factors)    
-    # error_global_scaled = error_global / np.sqrt(scale_factor)
-    
+
     # Populate dictionary with proper fields
     out[i[0]][i[1]]["error_node_3d"] = error_node_3d
     out[i[0]][i[1]]["error_node_2d"] = error_node_2d
     out[i[0]][i[1]]["error_node_1d"] = error_node_1d
     out[i[0]][i[1]]["error_edge_2d"] = error_edge_2d
     out[i[0]][i[1]]["error_edge_1d"] = error_edge_1d
-    out[i[0]][i[1]]["error_edge_0d"] = error_edge_0d  
+    out[i[0]][i[1]]["error_edge_0d"] = error_edge_0d
     out[i[0]][i[1]]["scaled_majorant"] = scaled_majorant
-
-    # # Print info in the console
-    # print("SUMMARY OF ERRORS")
-    # print('Error node 3D:', error_node_3d)
-    # print('Error node 2D:', error_node_2d)
-    # print('Error node 1D:', error_node_1d)
-    # print('Error edge 2D:', error_edge_2d)
-    # print('Error edge 1D:', error_edge_1d)
-    # print('Error edge 0D:', error_edge_0d)
-    # print('Scaled global error:', error_global_scaled)
-    # print('Global error:', error_global)
-    # print('\n')
 
 #%% Export
 # Permutations
 rows = len(numerical_methods) * len(mesh_resolutions)
 
 # Intialize lists
-numerical_method_name = [ ]
-mesh_resolution_name = [ ]
-col_3d_node = [ ]
-col_2d_node = [ ]
-col_1d_node = [ ]
-col_2d_edge = [ ]
-col_1d_edge = [ ]
-col_0d_edge = [ ]
-col_scaled_majorant = [ ]
+numerical_method_name = []
+mesh_resolution_name = []
+col_3d_node = []
+col_2d_node = []
+col_1d_node = []
+col_2d_edge = []
+col_1d_edge = []
+col_0d_edge = []
+col_scaled_majorant = []
 
 # Populate lists
 for i in itertools.product(numerical_methods, mesh_resolutions):
@@ -378,109 +331,44 @@ for i in itertools.product(numerical_methods, mesh_resolutions):
     col_scaled_majorant.append(out[i[0]][i[1]]["scaled_majorant"])
 
 # Prepare for exporting
-export = np.zeros(rows, 
-              dtype=[('var1', 'U6'), ('var2', 'U6'), 
-                      ('var3', float), ('var4', float), 
-                      ('var5', float), ('var6', float), 
-                      ('var7', float), ('var8', float),
-                      ('var9', float),
-                      ])
+export = np.zeros(
+    rows,
+    dtype=[
+        ("var1", "U6"),
+        ("var2", "U6"),
+        ("var3", float),
+        ("var4", float),
+        ("var5", float),
+        ("var6", float),
+        ("var7", float),
+        ("var8", float),
+        ("var9", float),
+    ],
+)
 
-export['var1'] = numerical_method_name
-export['var2'] = mesh_resolution_name
-export['var3'] = col_3d_node
-export['var4'] = col_2d_node
-export['var5'] = col_1d_node
-export['var6'] = col_2d_edge
-export['var7'] = col_1d_edge
-export['var8'] = col_0d_edge
-export['var9'] = col_scaled_majorant
+export["var1"] = numerical_method_name
+export["var2"] = mesh_resolution_name
+export["var3"] = col_3d_node
+export["var4"] = col_2d_node
+export["var5"] = col_1d_node
+export["var6"] = col_2d_edge
+export["var7"] = col_1d_edge
+export["var8"] = col_0d_edge
+export["var9"] = col_scaled_majorant
 
 # Header
-header = "num_method mesh_size eta_omega_3d eta_omega_2d eta_omega_1d" 
+header = "num_method mesh_size eta_omega_3d eta_omega_2d eta_omega_1d"
 header += " eta_gamma_2d eta_gamma_1d eta_gamma_0d scaled_majorant"
 
 # Write into txt
-np.savetxt('convergence_geiger.txt', 
-            export,
-            delimiter=',',
-            fmt="%4s %8s %2.2e %2.2e %2.2e %2.2e %2.2e %2.2e %2.2e",
-            header=header
-            )
+np.savetxt(
+    "convergence_bench3d.txt",
+    export,
+    delimiter=",",
+    fmt="%4s %8s %2.2e %2.2e %2.2e %2.2e %2.2e %2.2e %2.2e",
+    header=header,
+)
 
-#%% Export to LaTeX
-
-# Permutations
-rows = len(numerical_methods) * len(mesh_resolutions)
-
-# Intialize lists
-ampersend = []
-for i in range(rows): ampersend.append('&')
-numerical_method_name = [ ]
-mesh_resolution_name = [ ]
-col_3d_node = [ ]
-col_2d_node = [ ]
-col_1d_node = [ ]
-col_2d_edge = [ ]
-col_1d_edge = [ ]
-col_0d_edge = [ ]
-col_major = [ ]
-
-# Populate lists
-for i in itertools.product(numerical_methods, mesh_resolutions):
-    numerical_method_name.append(i[0])
-    mesh_resolution_name.append(i[1])
-    col_3d_node.append(out[i[0]][i[1]]["error_node_3d"])
-    col_2d_node.append(out[i[0]][i[1]]["error_node_2d"])
-    col_1d_node.append(out[i[0]][i[1]]["error_node_1d"])
-    col_2d_edge.append(out[i[0]][i[1]]["error_edge_2d"])
-    col_1d_edge.append(out[i[0]][i[1]]["error_edge_1d"])
-    col_0d_edge.append(out[i[0]][i[1]]["error_edge_0d"])
-    col_scaled_majorant.append(out[i[0]][i[1]]["majorant_scaled"])
-
-# Prepare for exporting
-export = np.zeros(rows, 
-              dtype=[('var1', 'U6'), ('var2', 'U6'), 
-                      ('var3', float), ('amp1', 'U6'), 
-                      ('var4', float), ('amp2', 'U6'),
-                      ('var5', float), ('amp3', 'U6'),
-                      ('var6', float), ('amp4', 'U6'),
-                      ('var7', float), ('amp5', 'U6'),
-                      ('var8', float), ('amp6', 'U6'),
-                      ('var9', float), 
-                      ])
-
-export['var1'] = numerical_method_name
-export['var2'] = mesh_resolution_name
-export['var3'] = col_3d_node
-export['amp1'] = ampersend
-export['var4'] = col_2d_node
-export['amp2'] = ampersend
-export['var5'] = col_1d_node
-export['amp3'] = ampersend
-export['var6'] = col_2d_edge
-export['amp4'] = ampersend
-export['var7'] = col_1d_edge
-export['amp5'] = ampersend
-export['var8'] = col_0d_edge
-export['amp6'] = ampersend
-export['var9'] = col_scaled_majorant
-
-# Format string
-fmt = "%4s %8s %2.2e %1s %2.2e %1s %2.2e %1s %2.2e %1s %2.2e "
-fmt += "%1s %2.2e %1s %2.2e"
-
-# Header
-header = "num_method mesh_size eta_omega_3d & eta_omega_2d & eta_omega_1d & "
-heaader =+ "eta_gamma_2d & eta_gamma_1d & eta_gamma_0d & scaled_majorant"
-
-# Write into txt
-np.savetxt('convergence_geiger_tex.txt', 
-            export,
-            delimiter=',',
-            fmt=fmt,
-            header=header
-            )
 
 #%% Exporting to Paraview
 g_3d = gb.grids_of_dimension(3)[0]
@@ -488,7 +376,6 @@ tol = 1e-8
 b_faces = g_3d.tags["domain_boundary_faces"].nonzero()[0]
 bc_val = np.zeros(g_3d.num_faces)
 if b_faces.size != 0:
-
     b_face_centers = g_3d.face_centers[:, b_faces]
     b_inflow = np.logical_and.reduce(
         tuple(b_face_centers[i, :] < 0.25 - tol for i in range(3))
@@ -498,37 +385,16 @@ if b_faces.size != 0:
     )
 
 cells_inflow = np.unique(sps.find(g_3d.cell_faces[b_faces[b_inflow]])[1])
-cells_outflow  = np.unique(sps.find(g_3d.cell_faces[b_faces[b_outflow]])[1])
+cells_outflow = np.unique(sps.find(g_3d.cell_faces[b_faces[b_outflow]])[1])
 cell_boundaries = np.zeros(g_3d.num_cells)
 cell_boundaries[cells_inflow] = -2
 cell_boundaries[cells_outflow] = -1
 
 for g, d in gb:
     if g.dim == gb.dim_max():
-        d[pp.STATE]['cell_bound'] = cell_boundaries
+        d[pp.STATE]["cell_bound"] = cell_boundaries
     else:
-        d[pp.STATE]['cell_bound'] = np.zeros(g.num_cells)
+        d[pp.STATE]["cell_bound"] = np.zeros(g.num_cells)
 
-exporter = pp.Exporter(gb, "fine", folder_name="out")
-exporter.write_vtu(['pressure', 'diffusive_error', 'scaled_error', "cell_bound"])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+paraview = pp.Exporter(gb, "bench3d", folder_name="out")
+paraview.write_vtu(["pressure", "diffusive_error", "cell_bound"])
