fb402842c4e072f56eac9523a1890d69d80afea4
Jhabriel Varela <jhabriel@gmail.com>
2021-11-24 15:50:08 +0100

    Improved doc + black + flake8

--- paper_examples/ex51_validation2D/analytical_2d.py
+++ paper_examples/ex51_validation2D/analytical_2d.py
@@ -8,7 +8,6 @@ from typing import Tuple
 
 
 class ExactSolution2D:
-
     def __init__(self, gb: pp.GridBucket):
 
         # Grid bucket
@@ -30,13 +29,17 @@ class ExactSolution2D:
 
         # Get list of cell indices
         bottom_cells = self.g2d.cell_centers[1] < 0.25
-        middle_cells = (self.g2d.cell_centers[1] >= 0.25) & (self.g2d.cell_centers[1] <= 0.75)
+        middle_cells = (self.g2d.cell_centers[1] >= 0.25) & (
+            self.g2d.cell_centers[1] <= 0.75
+        )
         top_cells = self.g2d.cell_centers[1] > 0.75
         self.cell_idx: list = [bottom_cells, middle_cells, top_cells]
 
         # Get list of boundary indices
         bottoms_bc = self.g2d.face_centers[1] < 0.25
-        middle_bc = (self.g2d.face_centers[1] >= 0.25) & (self.g2d.face_centers[1] <= 0.75)
+        middle_bc = (self.g2d.face_centers[1] >= 0.25) & (
+            self.g2d.face_centers[1] <= 0.75
+        )
         top_bc = self.g2d.face_centers[1] > 0.75
         self.bc_idx: list = [bottoms_bc, middle_bc, top_bc]
 
@@ -57,8 +60,9 @@ class ExactSolution2D:
 
         Parameters
         ----------
-        which (str): "cc" for the cell center evaluation, "sym" for the symbolic
-            expressions, and "fun" for the lambda functions depending on (x, y).
+        which (str): "cc" for the cell center evaluation, "sym" for the
+            symbolic expressions, and "fun" for the lambda functions
+            depending on (x, y).
 
         Returns
         -------
@@ -70,7 +74,9 @@ class ExactSolution2D:
         x, y = sym.symbols("x y")
 
         p2_top = self._dist_top ** (1 + self._alpha)
-        p2_mid = self._dist_mid ** (1 + self._alpha) + self._bubble * self._dist_mid
+        p2_mid = (
+            self._dist_mid ** (1 + self._alpha) + self._bubble * self._dist_mid
+        )
         p2_bot = self._dist_bot ** (1 + self._alpha)
 
         p2_sym = [p2_bot, p2_mid, p2_top]
@@ -92,8 +98,9 @@ class ExactSolution2D:
 
         Parameters
         ----------
-        which (str): "cc" for the cell center evaluation, "sym" for the symbolic
-            expressions, and "fun" for the lambda functions depending on (x, y).
+        which (str): "cc" for the cell center evaluation, "sym" for the
+            symbolic expressions, and "fun" for the lambda functions
+            depending on (x, y).
 
         Returns
         -------
@@ -103,12 +110,14 @@ class ExactSolution2D:
         x, y = sym.symbols("x y")
         cc = self.g2d.cell_centers
 
-        gradp2_sym = [[sym.diff(p, x), sym.diff(p, y)] for p in self.p2d(which="sym")]
+        gradp2_sym = [
+            [sym.diff(p, x), sym.diff(p, y)] for p in self.p2d(which="sym")
+        ]
 
         gradp2_fun = [
             [
                 sym.lambdify((x, y), gradp[0], "numpy"),
-                sym.lambdify((x, y), gradp[1], "numpy")
+                sym.lambdify((x, y), gradp[1], "numpy"),
             ]
             for gradp in gradp2_sym
         ]
@@ -134,8 +143,9 @@ class ExactSolution2D:
 
         Parameters
         ----------
-        which (str): "cc" for the cell center evaluation, "sym" for the symbolic
-            expressions, and "fun" for the lambda functions depending on (x, y).
+        which (str): "cc" for the cell center evaluation, "sym" for the
+            symbolic expressions, and "fun" for the lambda functions
+            depending on (x, y).
 
         Returns
         -------
@@ -146,12 +156,14 @@ class ExactSolution2D:
         x, y = sym.symbols("x y")
         cc = self.g2d.cell_centers
 
-        u2_sym = [[-sym.diff(p, x), -sym.diff(p, y)] for p in self.p2d(which="sym")]
+        u2_sym = [
+            [-sym.diff(p, x), -sym.diff(p, y)] for p in self.p2d(which="sym")
+        ]
 
         u2_fun = [
             [
                 sym.lambdify((x, y), u[0], "numpy"),
-                sym.lambdify((x, y), u[1], "numpy")
+                sym.lambdify((x, y), u[1], "numpy"),
             ]
             for u in u2_sym
         ]
@@ -177,8 +189,9 @@ class ExactSolution2D:
 
         Parameters
         ----------
-        which (str): "cc" for the cell center evaluation, "sym" for the symbolic
-            expressions, and "fun" for the lambda functions depending on (x, y).
+        which (str): "cc" for the cell center evaluation, "sym" for the
+            symbolic expressions, and "fun" for the lambda functions
+            depending on (x, y).
 
         Returns
         -------
@@ -188,7 +201,10 @@ class ExactSolution2D:
         x, y = sym.symbols("x y")
         cc = self.g2d.cell_centers
 
-        f2_sym = [sym.diff(u[0], x) + sym.diff(u[1], y) for u in self.u2d(which="sym")]
+        f2_sym = [
+            sym.diff(u[0], x) + sym.diff(u[1], y)
+            for u in self.u2d(which="sym")
+        ]
 
         f2_fun = [sym.lambdify((x, y), f, "numpy") for f in f2_sym]
 
@@ -209,8 +225,9 @@ class ExactSolution2D:
 
         Parameters
         ----------
-        which (str): "cc" for the cell center evaluation, "sym" for the symbolic
-            expressions, and "fun" for the lambda functions depending on (y).
+        which (str): "cc" for the cell center evaluation, "sym" for the
+            symbolic expressions, and "fun" for the lambda functions
+            depending on (y).
 
         Returns
         -------
@@ -239,8 +256,9 @@ class ExactSolution2D:
 
         Parameters
         ----------
-        which (str): "cc" for the cell center evaluation, "sym" for the symbolic
-           expressions, and "fun" for the lambda functions depending on (y).
+        which (str): "cc" for the cell center evaluation, "sym" for the
+            symbolic expressions, and "fun" for the lambda functions
+            depending on (y).
 
         Returns
         -------
@@ -259,16 +277,10 @@ class ExactSolution2D:
         # Cell center pressures (in real coordinates)
         p1_cc = p1_fun(cc[1])
 
-        # Cell center pressures (in rotated coordinates)
-        p1 = np.array([np.zeros(self.g1d.num_cells), p1_cc, np.zeros(self.g1d.num_cells)])
-        p1_rot_cc = np.dot(self.g1d_rot.rotation_matrix, p1)[self.g1d_rot.dim_bool][0]
-
         if which == "sym":
             return p1_sym
         elif which == "fun":
             return p1_fun
-        elif which == "cc_rot":
-            return p1_rot_cc
         else:
             return p1_cc
 
@@ -278,8 +290,9 @@ class ExactSolution2D:
 
         Parameters
         ----------
-        which (str): "cc" for the cell center evaluation, "sym" for the symbolic
-           expressions, and "fun" for the lambda functions depending on (y).
+        which (str): "cc" for the cell center evaluation, "sym" for the
+            symbolic expressions, and "fun" for the lambda functions
+            depending on (y).
 
         Returns
         -------
@@ -308,8 +321,9 @@ class ExactSolution2D:
 
         Parameters
         ----------
-        which (str): "cc" for the cell center evaluation, "sym" for the symbolic
-           expressions, and "fun" for the lambda functions depending on (y).
+        which (str): "cc" for the cell center evaluation, "sym" for the
+            symbolic expressions, and "fun" for the lambda functions
+            depending on (y).
 
         Returns
         -------
@@ -338,8 +352,9 @@ class ExactSolution2D:
 
         Parameters
         ----------
-        which (str): "cc" for the cell center evaluation, "sym" for the symbolic
-           expressions, and "fun" for the lambda functions depending on (y).
+        which (str): "cc" for the cell center evaluation, "sym" for the
+            symbolic expressions, and "fun" for the lambda functions
+            depending on (y).
 
         Returns
         -------
@@ -366,7 +381,7 @@ class ExactSolution2D:
 
     def dir_bc_values(self):
         """
-        Computation of the exact fracture source
+        Computation of the Dirichlet data for the bulk
 
         Returns
         -------
@@ -397,6 +412,7 @@ class ExactSolution2D:
             # Declare integrand
             def integrand(x):
                 return f(x[0], x[1]) * np.ones_like(x[0])
+
             # Integrate, and add the contribution of each subregion
             integral += int_method.integrate(integrand, elements) * idx
 
@@ -404,7 +420,7 @@ class ExactSolution2D:
 
     def integrate_f1d(self):
 
-        method = qp.c1.newton_cotes_closed(5)
+        method = qp.c1.newton_cotes_closed(10)
         g_rot = utils.rotate_embedded_grid(self.g1d)
         elements = utils.get_quadpy_elements(self.g1d, g_rot)
         elements *= -1  # we have to use the real y coordinates here
