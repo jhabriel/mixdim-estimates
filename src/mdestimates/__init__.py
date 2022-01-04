784bb33d1b54e97db00e31c7e420de653f3bafa5
Jhabriel Varela <jhabriel.varela@uib.no>
2022-01-04 02:35:54 +0100

    DEV: Include rotated grids as a separate class

--- src/mdestimates/__init__.py
+++ src/mdestimates/__init__.py
@@ -2,3 +2,4 @@ __all__ = []
 from ._class import ErrorEstimate
 from .pressure_reconstruction import PressureReconstruction
 from .velocity_reconstruction import VelocityReconstruction
+from .grid_rotation import RotatedGrid
