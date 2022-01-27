fdef461e702f9fa8117e4129d83dbbe33430c9da
Jhabriel Varela <jhabriel.varela@uib.no>
2022-01-27 18:29:49 +0100

    DEV: Add DiffusiveError and ResidualError classes to __init__

--- src/mdestimates/__init__.py
+++ src/mdestimates/__init__.py
@@ -2,4 +2,6 @@ __all__ = []
 from ._class import ErrorEstimate
 from .pressure_reconstruction import PressureReconstruction
 from .velocity_reconstruction import VelocityReconstruction
+from .diffusive_error import DiffusiveError
+from .residual_error import ResidualError
 from .grid_rotation import RotatedGrid
