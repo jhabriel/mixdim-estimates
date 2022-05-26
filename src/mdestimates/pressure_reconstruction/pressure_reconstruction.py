24eca640e853f614a032ffa18e867801cb8baaa9
Jhabriel Varela <jhabriel@gmail.com>
2022-05-26 10:18:37 +0200

    DEV: Skeleton for other pressure reconstruction methods

--- src/mdestimates/pressure_reconstruction/pressure_reconstruction.py
+++ src/mdestimates/pressure_reconstruction/pressure_reconstruction.py
@@ -53,12 +53,15 @@ class PressureReconstruction(mde.ErrorEstimate):
             g_rot: mde.RotatedGrid = mde.RotatedGrid(g)
 
             # Obtain Lagrangian coordinates
-            if self.p_recon_method == "inv_local_gradp":
-                point_val, point_coo = self.inverse_local_gradp(g, g_rot, d)
-            elif self.p_recon_method == "direct_reconstruction":
-                raise NotImplementedError
+            if self.p_recon_method == "keilegavlen":
+                point_val, point_coo = self.keilegavlen_p1(g, g_rot, d)
+            elif self.p_recon_method == "cochez-dhondt":
+                point_val, point_coo = self.cochez_p1(g, g_rot, d)
+            elif self.p_recon_method == "vohralik":
+                point_val, point_coo = self.vohralik_p2(g, g_rot, d)
             else:
-                raise ValueError("Pressure reconstruction method not implemented.")
+                raise ValueError("Pressure reconstruction method not "
+                                 "available.")
 
             # Obtain pressure coefficients
             recons_p = utils.interpolate_P1(point_val, point_coo)
@@ -72,7 +75,103 @@ class PressureReconstruction(mde.ErrorEstimate):
 
         return None
 
-    def inverse_local_gradp(self, g: pp.Grid, g_rot: mde.RotatedGrid, d: dict):
+    def cochez_p1(self, g: pp.Grid, g_rot: mde.RotatedGrid, d: dict):
+        """
+        Pressure reconstruction using average of P0 potentials over patches.
+
+        Parameters
+        ----------
+        g : PorePy object
+            Grid
+        g_rot: Rotated grid object
+            Rotated pseudo-grid
+        d : Dictionary
+            Dicitionary containing the parameters
+
+        Raises
+        ------
+        Value Error:
+            If pressure solution is not in d[pp.STATE]
+            If pressure solution does not have the correct size
+
+        Returns
+        -------
+        [point_val, point_coo]: Tuple
+            List containing the pressures and coordinates at the Lagrangian points
+
+        """
+
+        # Sanity checks
+        if self.p_name not in d[pp.STATE]:
+            raise ValueError("Pressure solution not found.")
+
+        if d[pp.STATE][self.p_name].size != g.num_cells:
+            raise ValueError("Inconsistent size of pressure solution.")
+
+        # Retrieve P0 cell-center pressure
+        p_cc = d[pp.STATE][self.p_name].copy()
+
+        # Retrieving topological data
+        nc = g.num_cells
+        nf = g.num_faces
+        nn = g.num_nodes
+
+        # Perform reconstruction
+        cell_nodes = g.cell_nodes()
+        cell_node_volumes = cell_nodes * sps.dia_matrix((g.cell_volumes, 0), (nc, nc))
+        sum_cell_nodes = cell_node_volumes * np.ones(nc)
+        cell_nodes_scaled = (
+                sps.dia_matrix((1.0 / sum_cell_nodes, 0), (nn, nn)) * cell_node_volumes
+        )
+
+        cell_nodes_pressure = (
+            sps.dia_matrix((1.0 / sum))
+        )
+
+
+        # Obtaining nodal pressures
+        cell_nodes_map, _, _ = sps.find(g.cell_nodes())
+        cell_node_matrix = cell_nodes_map.reshape(np.array([g.num_cells, g.dim + 1]))
+        nodal_pressures = np.zeros(nn)
+
+        for col in range(g.dim + 1):
+            nodes = cell_node_matrix[:, col]
+            dist = g_rot.nodes[: g.dim, nodes] - g_rot.cell_centers[: g.dim]
+            scaling = cell_nodes_scaled[nodes, np.arange(nc)]
+            contribution = (
+                    np.asarray(scaling) * (p_cc + np.sum(dist * loc_grad, axis=0))
+            ).ravel()
+            nodal_pressures += np.bincount(nodes, weights=contribution, minlength=nn)
+
+        # Treatment of boundary conditions
+        # TODO: Check this: We should only look for Dirichlet bc at the ambient grid
+        bc = d[pp.PARAMETERS][self.kw]["bc"]
+        bc_values = d[pp.PARAMETERS][self.kw]["bc_values"]
+        external_dirichlet_boundary = np.logical_and(
+            bc.is_dir, g.tags["domain_boundary_faces"]
+        )
+        face_vec = np.zeros(nf)
+        face_vec[external_dirichlet_boundary] = 1
+        num_dir_face_of_node = g.face_nodes * face_vec
+        is_dir_node = num_dir_face_of_node > 0
+        face_vec *= 0
+        face_vec[external_dirichlet_boundary] = bc_values[external_dirichlet_boundary]
+        node_val_dir = g.face_nodes * face_vec
+        node_val_dir[is_dir_node] /= num_dir_face_of_node[is_dir_node]
+        nodal_pressures[is_dir_node] = node_val_dir[is_dir_node]
+
+        # Save in the dictionary
+        d[self.estimates_kw]["node_pressure"] = nodal_pressures
+
+        # Export lagrangian nodes and coordinates
+        cell_nodes_map, _, _ = sps.find(g.cell_nodes())
+        nodes_cell = cell_nodes_map.reshape(np.array([g.num_cells, g.dim + 1]))
+        point_val = nodal_pressures[nodes_cell]
+        point_coo = g_rot.nodes[:, nodes_cell]
+
+        return point_val, point_coo
+
+    def keilegavlen_p1(self, g: pp.Grid, g_rot: mde.RotatedGrid, d: dict):
         """
         Pressure reconstruction using the inverse of the numerical fluxes.
         Author: Eirik Keilegavlen
@@ -209,6 +308,9 @@ class PressureReconstruction(mde.ErrorEstimate):
         point_coo = g_rot.nodes[:, nodes_cell]
 
         return point_val, point_coo
+
+    def vohralik_p2(self, g: pp.Grid, g_rot: mde.RotatedGrid, d: dict):
+        pass
 
     def _test_pressure_reconstruction(self, g, recon_p, point_val, point_coo):
         """
